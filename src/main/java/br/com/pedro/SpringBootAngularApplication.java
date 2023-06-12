0e0d7d699a99a01530c7d91c2e070fc9907f7d5f
Pedro Henrique Eichler <piterhme@gmail.com>
2023-06-12 15:19:15 -0300

    implementação inicial do projeto spring boot

---
+++ src/main/java/br/com/pedro/SpringBootAngularApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularApplication.class, args);
+	}
+
+}
